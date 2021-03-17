56a0f7ed1087a5273bc9a38e4cc9495977c40ada
hilmi genc <h.genc@w-h.net>
2021-03-17 09:53:39 +0100

    Create backend with strapi

---
+++ myalbum_backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '40db9e28aa83f5b3510b040640068b74'),
+    },
+  },
+});
---
+++ myalbum_backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd8e3c50f-49d4-4145-8347-9df78cc3ba6c'
+};
